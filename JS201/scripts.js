bbe5aed01a6a1930096df2a3f4b77662093e1298
Vasco Faria <125152469+Vasco-Faria@users.noreply.github.com>
2023-07-28 16:21:48 +0100

    Add files via upload

---
+++ JS201/scripts.js
@@ -0,0 +1,12 @@
+// const varName = "thing";
+// const myName = "Vasco";
+
+// if (something == "something") {
+//   // our code in here
+// } else {
+//   // something else in here
+// }
+
+// function theName(param1, param2) {
+//   // our code in here
+// }
