5730811766be2a327cf70fff9753eda308f7283e
Vasco Faria <125152469+Vasco-Faria@users.noreply.github.com>
2023-09-28 20:04:28 +0100

    Django 201

---
+++ Django 201/profiles/views.py
@@ -0,0 +1,94 @@
+from django.contrib.auth.models import User
+from django.views.generic import DetailView, View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from feed.models import Post
+from followers.models import Follower
+from .forms import UserUpdateForm,ProfileUpdateForm
+
+
+class ProfileDetailView(DetailView):
+    http_method_names = ["get"]
+    template_name = "profiles/detail.html"
+    model = User
+    context_object_name = "user"
+    slug_field = "username"
+    slug_url_kwarg = "username"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        user = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context['total_posts'] = Post.objects.filter(author=user).count()
+        context['total_followers'] = Follower.objects.filter(following=user).count()
+        # context['total_followers'] = ...
+        if self.request.user.is_authenticated:
+            context['you_follow'] = Follower.objects.filter(following=user, followed_by=self.request.user).exists()
+        return context
+
+
+class FollowView(LoginRequiredMixin, View):
+    http_method_names = ["post"]
+
+    def post(self, request, *args, **kwargs):
+        data = request.POST.dict()
+
+        if "action" not in data or "username" not in data:
+            return HttpResponseBadRequest("Missing data")
+
+        try:
+            other_user = User.objects.get(username=data['username'])
+        except User.DoesNotExist:
+            return HttpResponseBadRequest("Missing user")
+
+        if data['action'] == "follow":
+            # Follow
+            follower, created = Follower.objects.get_or_create(
+                followed_by=request.user,
+                following=other_user
+            )
+        else:
+            # Unfollow
+            try:
+                follower = Follower.objects.get(
+                    followed_by=request.user,
+                    following=other_user,
+                )
+            except Follower.DoesNotExist:
+                follower = None
+
+            if follower:
+                follower.delete()
+
+        return JsonResponse({
+            'success': True,
+            'wording': "Unfollow" if data['action'] == "follow" else "Follow"
+        })
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES ,instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request,f'Your Profile is Updated !!')
+            return redirect('profile')            
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form':u_form,
+        'p_form': p_form
+    }
+    return render(request,'profiles/update_profile.html', context)
